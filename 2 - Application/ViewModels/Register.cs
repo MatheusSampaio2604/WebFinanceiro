254ef79c589470204d444069962b528bd9b7618e
Matheus Sampaio <matheusdecarvalhosampaio@gmail.com>
2023-12-26 15:38:19 -0300

    .

---
+++ 2 - Application/ViewModels/Register.cs
@@ -0,0 +1,20 @@
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [PasswordPropertyText]
+        [Required]
+        [MinLength(10)]
+        public required string Senha { get; set; }
+        public string? CPF { get; set; }
+        public string? Phone { get; set; }
+    }
+}
