9f7bd9ee51c946a0b53df16bdf0f3d91837bdb9d
Matheus Sampaio <matheusdecarvalhosampaio@gmail.com>
2023-12-28 11:51:42 -0300

    ...

--- 3 - Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ 3 - Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56550f5089c5cd8d7f7ed892259c8a64ac2e4708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
